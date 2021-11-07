a6af1e1a7b78bf72e60ec168539a083df65b5779
Josh Bickett <josh@bickett.net>
2021-11-07 21:34:53 +0000

    minor updates

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/app/venv"
+set -gx VIRTUAL_ENV "/home/makememe.ai/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
