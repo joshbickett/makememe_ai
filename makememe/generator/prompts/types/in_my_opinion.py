8fe6378c362b553ef7ac03ba77192288a680e211
Josh Bickett <josh@bickett.net>
2021-12-04 18:34:30 -0800

    Rotate Text

--- makememe/generator/prompts/types/in_my_opinion.py
+++ makememe/generator/prompts/types/in_my_opinion.py
@@ -1,5 +1,5 @@
 from makememe.generator.prompts.prompt import Prompt
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageOps
 import datetime
 from makememe.generator.prompts.helper import Helper
 from makememe.generator.design.font import font_path
@@ -39,24 +39,41 @@ Message: If only we were more kind to each other, this world would be a more ple
 '''
 
     def create(self, meme_text):
-        with Image.open(f"makememe/static/meme_pics/{self.name.lower()}.jpg").convert("RGBA") as base:
-            txt = Image.new("RGBA", base.size, (0, 0, 0, 0))
+        # with Image.open(f"makememe/static/meme_pics/{self.name.lower()}.jpg").convert("RGBA") as base:
+
+        base=Image.open(f"makememe/static/meme_pics/{self.name.lower()}.jpg")
+
+        wrapped_text = Helper.wrap(meme_text['opinion'], 25)
+
+        font = ImageFont.truetype(font_path, 25)
+        txt=Image.new('L', (500,50))
+        d = ImageDraw.Draw(txt)
+        d.text( (0, 0), wrapped_text,  font=font, fill=255)
+        w=txt.rotate(20,  expand=1)
+
+        base.paste( ImageOps.colorize(w, (0,0,0), (0,0,0)), (600,360),  w)
+
+        date = datetime.datetime.now()
+        image_name = f'{date}.jpg'
+        file_location = f'makememe/static/creations/{image_name}'
+        base.save(file_location)
+        return image_name
 
 
-            font = ImageFont.truetype(font_path, 25)
-            watermark_font = ImageFont.truetype(font_path, 15)
-            d = ImageDraw.Draw(txt)
+        # font = ImageFont.truetype(font_path, 25)
+        # watermark_font = ImageFont.truetype(font_path, 15)
+        # d = ImageDraw.Draw(txt)
 
-            wrapped_text = Helper.wrap(meme_text['opinion'], 25)
+        
+        
 
-            d.text((600, 360),wrapped_text, font=font, fill=(0, 0, 0, 255))
-            d.text((10, 500), "makememe.ai", font=watermark_font, fill=(255, 255, 255, 128))
-            out = Image.alpha_composite(base, txt)
-            if out.mode in ("RGBA", "P"):
-                out = out.convert("RGB")
-                date = datetime.datetime.now()
-                image_name = f'{date}.jpg'
-                file_location = f'makememe/static/creations/{image_name}'
-                out.save(file_location)
-                return image_name
+        # d.text((600, 360),wrapped_text, font=font, fill=(0, 0, 0, 255))
+        # d.text((10, 500), "makememe.ai", font=watermark_font, fill=(255, 255, 255, 128))
+        
+        # w = txt.rotate(17.5, expand=1)
+        # # out = Image.alpha_composite(base, w)
+        # base.paste(w)
+        # if base.mode in ("RGBA", "P"):
+        #     base = base.convert("RGB")
+        
 
