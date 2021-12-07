cff90838025d57ca8349c7a2b3d83188b54baa62
Josh Bickett <josh@bickett.net>
2021-12-07 10:39:37 -0800

    Update to new image and text display method

--- makememe/generator/design/image_manager.py
+++ makememe/generator/design/image_manager.py
@@ -1,4 +1,5 @@
-from PIL import Image, ImageDraw, ImageFont, ImageOps
+import os, sys
+from PIL import Image, ImageDraw, ImageFont, ImageOps, ImageChops
 from makememe.generator.design.font import font_path
 from makememe.generator.prompts.helper import Helper
 
@@ -8,22 +9,25 @@ class Image_Manager:
     print("Image manager create")
   
   @staticmethod
-  def add_text(base, text, position, font_size, text_color, text_width_proportion=4, wrapped_width=None, rotate_degrees=None):
+  def add_text(base, text, position, font_size, text_color="black", text_width_proportion=4, wrapped_width=None, rotate_degrees=None):
+  
+    try: 
+      overlay_image = Image.new("RGBA", base.size, (0, 0, 0, 0))
+      if wrapped_width is not None: 
+        text = Helper.wrap(text, wrapped_width)
 
-    if wrapped_width is not None: 
-      text = Helper.wrap(text, wrapped_width)
+      font = ImageFont.truetype(font_path,font_size)
+      draw = ImageDraw.Draw(overlay_image)
+      draw.text(position, text, font=font, fill=(0, 0, 0, 255))
+      if rotate_degrees is not None: 
+        overlay_image = overlay_image.rotate(rotate_degrees)
 
-    font = ImageFont.truetype(font_path, font_size)
-    overlay_image =Image.new('L', (int(base.width/text_width_proportion), int(base.height/4)), 0)
-    draw = ImageDraw.Draw(overlay_image)    
-    draw.text((0, 0), text,  font=font, fill=255)
-
-    if rotate_degrees is not None:
-      overlay_image=overlay_image.rotate(rotate_degrees,  expand=1)
-
-    color_value = (0,0,0)
-    if text_color == "white": 
-      color_value = (255,255,255)
-    base.paste(ImageOps.colorize(overlay_image, (0,0,0), color_value), position, overlay_image)
-
-    return base
+      return overlay_image
+    except Exception as e:
+      exc_type, exc_obj, exc_tb = sys.exc_info()
+      fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+      print(f'error: {e}')
+      print(f'line: {exc_tb.tb_lineno}')
+      print(f'file: {fname}')
+      return "error"
+    
--- makememe/generator/prompts/types/equal_in_comparison.py
+++ makememe/generator/prompts/types/equal_in_comparison.py
@@ -1,8 +1,9 @@
 from makememe.generator.prompts.prompt import Prompt
-from PIL import Image, ImageDraw, ImageFont
 import datetime
-from makememe.generator.prompts.helper import Helper
+from PIL import Image, ImageDraw, ImageFont, ImageOps, ImageChops
 from makememe.generator.design.font import font_path
+from makememe.generator.prompts.helper import Helper
+from makememe.generator.design.image_manager import Image_Manager
 
 
 class Equal_In_Comparison(Prompt):
@@ -30,19 +31,21 @@ Message: Surfing in warm water is the same as surfing in cold water. They are eq
 '''
 
     def create(self, meme_text):
+
+        # base=Image.open(f"makememe/static/meme_pics/{self.name.lower()}.jpg")
+        # Image_Manager.add_text(base=base, text="makememe.ai", position=(10, 1150), font_size=20, text_color="black", wrapped_width=None, rotate_degrees=None)
+        # Image_Manager.add_text(base=base, text=meme_text['depiction'], position=(250, 725), font_size=30, text_color="black", text_width_proportion=2, wrapped_width=25, rotate_degrees=348)
+
         with Image.open(f"makememe/static/meme_pics/{self.name.lower()}.jpg").convert("RGBA") as base:
-            txt = Image.new("RGBA", base.size, (0, 0, 0, 0))
-            font = ImageFont.truetype(font_path,50)
-            watermark_font = ImageFont.truetype(font_path, 25)
-            d = ImageDraw.Draw(txt)
+            # txt = Image.new("RGBA", base.size, (0, 0, 0, 0))
+            # font = ImageFont.truetype(font_path,50)
+            # watermark_font = ImageFont.truetype(font_path, 25)
+            # d = ImageDraw.Draw(txt)
+            overlay_image = Image_Manager.add_text(base, meme_text['first'], (75, 200), 45, wrapped_width=15, rotate_degrees=350)
+            overlay_image_2 = Image_Manager.add_text(base, meme_text['second'], (575, 225), 45, wrapped_width=15, rotate_degrees=350)
 
-            meme_text_one = Helper.wrap(meme_text['first'], 15)
-            meme_text_two = Helper.wrap(meme_text['second'], 15)
-
-            d.text((150, 200), meme_text_one, font=font, fill=(0, 0, 0, 255))
-            d.text((600, 250), meme_text_two, font=font, fill=(0, 0, 0, 255))
-            d.text((10, 1300), "makememe.ai", font=watermark_font, fill=(0, 0, 0, 128))
-            out = Image.alpha_composite(base, txt)
+            base = Image.alpha_composite(base, overlay_image)
+            out = Image.alpha_composite(base, overlay_image_2)
             if out.mode in ("RGBA", "P"):
                 out = out.convert("RGB")
                 date = datetime.datetime.now()
