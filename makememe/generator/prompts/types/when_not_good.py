17411cd53d13316e63bee28addbe04a8cf340c63
Josh Bickett <josh@bickett.net>
2022-01-09 14:02:34 -0800

    Add another meme type

---
+++ makememe/generator/prompts/types/when_not_good.py
@@ -0,0 +1,35 @@
+from makememe.generator.prompts.prompt import Prompt
+import datetime
+from PIL import Image
+from makememe.generator.design.image_manager import Image_Manager
+
+class When_Not_Good(Prompt):
+    name = "When_Not_Good"
+    description = "when not good"
+
+    def __init__(self):
+        self.instruction = '''
+###
+Message: When all your friends are getting married and you've not been on a date.
+Meme:{"subject": "When all your friends are getting married and you've not been on a date."}
+###
+Message: I got a 4 year engineering degree and now can't remember calculus.
+Meme:{"subject": "When you get a 4 year engineering degree and now can't remember calculus."}
+###
+'''
+    def create(self, meme_text):
+        with Image.open(f"makememe/static/meme_pics/{self.name.lower()}.jpg").convert("RGBA") as base:
+
+            overlay_image = Image_Manager.add_text(base=base, text=meme_text['subject'], position=(100, 50), font_size=45, wrapped_width=40)
+            watermark = Image_Manager.add_text(base=base, text='makememe.ai', position=(10, 1000), font_size=20)
+
+            base = Image.alpha_composite(base, watermark)
+            out = Image.alpha_composite(base, overlay_image)
+            if out.mode in ("RGBA", "P"):
+                out = out.convert("RGB")
+                
+                date = datetime.datetime.now()
+                image_name = f'{date}.jpg'
+                file_location = f'makememe/static/creations/{image_name}'
+                out.save(file_location)
+                return image_name
