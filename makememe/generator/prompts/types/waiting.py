1d33a8d1d3f46fb4f94926fde7a76ffa0a904cdc
Josh Bickett <josh@bickett.net>
2022-03-20 00:44:13 -0700

    Improve waiting meme type

--- makememe/generator/prompts/types/waiting.py
+++ makememe/generator/prompts/types/waiting.py
@@ -11,22 +11,22 @@ class Waiting(Prompt):
         self.instruction = '''
 ###
 Message:I've been waiting for SpaceX to launch the starship for ever
-Meme:{"detail": "SpaceX Startship"}
+Meme:{"subject": "SpaceX Startship"}
 ###
 Message:I can't wait for makememe.ai to launch, but it's taking a little while
-Meme:{"detail": "makememe.ai"}
+Meme:{"subject": "makememe.ai"}
 ###
 Message:Drakes new album is going to be fire. Why do I have to wait
-Meme:{"detail": "Drakes new album"}
+Meme:{"subject": "Drakes new album"}
 ###
 Message:I want to create an NFT, but opensea.com is taking a while to load
-Meme:{"detail": "opensea.com"}
+Meme:{"subject": "opensea.com"}
 ###
 '''
     def create(self, meme_text):
         with Image.open(f"makememe/static/meme_pics/{self.name.lower()}.jpg").convert("RGBA") as base:
 
-            overlay_image = Image_Manager.add_text(base=base, text=meme_text['detail'], position=(600, 950), font_size=40, wrapped_width=20)
+            overlay_image = Image_Manager.add_text(base=base, text=meme_text['subject'], position=(600, 950), font_size=40, wrapped_width=20)
             watermark = Image_Manager.add_text(base=base, text='makememe.ai', position=(30, 1100), font_size=20)
             
             base = Image.alpha_composite(base, watermark)
