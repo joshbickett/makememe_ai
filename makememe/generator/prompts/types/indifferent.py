dfa3cbd01b65a96ed4c0fbf88bcc895aaa6d8e8d
Josh Bickett <josh@bickett.net>
2022-04-17 11:40:01 -0700

    Fix indifferent meme type

--- makememe/generator/prompts/types/indifferent.py
+++ makememe/generator/prompts/types/indifferent.py
@@ -6,7 +6,7 @@ from makememe.generator.design.image_manager import Image_Manager
 
 class Indifferent(Prompt):
     name = "Indifferent"
-    description = "I don't care about this"
+    description = "this is not important to me"
 
     def __init__(self):
         self.instruction = '''
